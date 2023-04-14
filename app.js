90be3e6ca86b03c74ce1b3fe7060829605a6e72c
Pablo <pgarciacardone@gmail.com>
2023-04-14 17:21:13 +0100

    Add further CSS styling
    Add services.html page
    Add styling to cards on services page
    Add mobile responsiveness to index and services pages

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 window.addEventListener("DOMContentLoaded", () => {
 });
 
+const toggleBtn = document.querySelector(".toggle");
+const navbar = document.querySelector(".navbar");
+
+toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
